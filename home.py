544b4692b8baf1142465468bdfd05a3500e8039f
joker <joker03081991@gmail.com>
2025-03-09 12:12:46 +0900

    add reset password feature

--- home.py
+++ home.py
@@ -89,7 +89,18 @@ class PriceScraperUI:
                 yahoo_products_df, 
                 use_container_width=True, 
                 height=height, 
-                key="scraped_product_list"
+                key="scraped_product_list",
+                column_config={
+                    "商品画像": st.column_config.ImageColumn(),
+                    "画像URL1": st.column_config.ImageColumn(),
+                    "画像URL2": st.column_config.ImageColumn(),
+                    "画像URL3": st.column_config.ImageColumn(),
+                    "画像URL4": st.column_config.ImageColumn(),
+                    "画像URL5": st.column_config.ImageColumn(),
+                    "画像URL6": st.column_config.ImageColumn(),
+                    "画像URL7": st.column_config.ImageColumn(),
+                    "画像URL8": st.column_config.ImageColumn(),
+                }
             )
         
         # Show progress if running
