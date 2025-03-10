88c653e7d773abb094c4208ba7b387a325feda4d
joker <joker03081991@gmail.com>
2025-03-10 18:16:17 +0900

    optimize full codes

--- home.py
+++ home.py
@@ -33,7 +33,7 @@ class PriceScraperUI:
                 
                 my_bar.progress(progress_value/limit, text=progress_text)
                 sleep(1)
-                if progress_value !=0 and progress_value == limit:
+                if progress_value !=0 and (progress_value % config.BATCH_SIZE == 0 or progress_value == limit):
                     st.rerun()
             my_bar.empty()
 
@@ -54,28 +54,27 @@ class PriceScraperUI:
     def _manage_product_list(self):
         yahoo_products_df = pd.DataFrame(columns=config.yahoo_columns)
 
-        if Path(config.SCRAPED_XLSX).exists():
-            saved_df = pd.read_excel(config.SCRAPED_XLSX)
-            for col in saved_df.columns:
-                if col in yahoo_products_df.columns:
-                    yahoo_products_df[col] = saved_df[col]
-
         if not self.running():
             uploaded_file = st.file_uploader("商品URLリスト", type="xlsx")
             if uploaded_file is not None:
                 new_df = pd.read_excel(uploaded_file)
 
-                if not new_df['商品URL'].equals(yahoo_products_df['商品URL']):
-                    for col in new_df.columns:
-                        if col in yahoo_products_df.columns:
-                            yahoo_products_df[col] = new_df[col]
-                
-                    st.write("T商品リストを読み込みました:", len(new_df))
-                    output_path = Path(config.SCRAPED_XLSX)
-                    output_path.parent.mkdir(parents=True, exist_ok=True)
-                    yahoo_products_df.to_excel(output_path, index=False)
-                    st.success(f'データを保存しました {output_path}')
+                for col in new_df.columns:
+                    if col in yahoo_products_df.columns:
+                        yahoo_products_df[col] = new_df[col]
+
+                st.write("T商品リストを読み込みました:", len(new_df))
+                output_path = Path(config.SCRAPED_XLSX)
+                output_path.parent.mkdir(parents=True, exist_ok=True)
+                yahoo_products_df.to_excel(output_path, index=False)
+                st.success(f'データを保存しました {output_path}')
             
+        if Path(config.SCRAPED_XLSX).exists():
+            df = pd.read_excel(config.SCRAPED_XLSX)
+            for col in df.columns:
+                if col in yahoo_products_df.columns:
+                    yahoo_products_df[col] = df[col]
+        
         yahoo_products_df.index = yahoo_products_df.index + 1
         height = min(len(yahoo_products_df) * 35 + 38, 700)
         
@@ -99,7 +98,6 @@ class PriceScraperUI:
             #     "画像URL8": st.column_config.ImageColumn(),
             # }
         )
-        
        
 
     def _setup_scraping_controls(self):
