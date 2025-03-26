162336ad6b966e65c6f3affd50d2cb60a34c02d8
JokerDev0308 <joker03081991@gmail.com>
2025-03-26 17:14:47 +0900

    Update yahoo auction because the update of page html

--- scripts/yahoo_auction1.py
+++ scripts/yahoo_auction1.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class YahooAuctionScraper1:
     def __init__(self):
-        self.driver = WebDriverManager.get_driver("yahoo_auction")
+        self.driver = WebDriverManager.get_driver("yahoo_auction1")
 
     
     def run(self, url):
