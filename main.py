04b466b2e21a7e6a687689903c62dbf04a27a906
JokerDev0308 <joker03081991@gmail.com>
2025-03-26 17:24:10 +0900

    Update yahoo auction because the update of page html

--- main.py
+++ main.py
@@ -114,9 +114,9 @@ class Scraper:
                     if 'auctions.yahoo.co.jp' in p_url:
                         if '/auctions.yahoo.co.jp/jp/auction/' in p_url:
                             p_url = p_url.replace('/auctions.yahoo.co.jp/jp/auction/', '/page.auctions.yahoo.co.jp/jp/auction/')
-                        try:
+                        if index == 0:
                             result = self.scraper_auction(p_url)
-                        except Exception as e:
+                        else:
                             result = self.scraper_auction1(p_url)
                     elif 'paypayfleamarket.yahoo.co.jp' in p_url:
                         result = self.scraper_fleaMarket(p_url)
